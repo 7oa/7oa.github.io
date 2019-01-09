48318602d5c28b494189c03ed37f9eb2dfbb5949
Ann Semenova <semenova.7oa@yandex.ru>
2019-01-09 11:12:35 +0300

    adaptive

--- js/freelancer.js
+++ js/freelancer.js
@@ -40,9 +40,33 @@ $('.navbar-collapse ul li a').click(function() {
 $('.sertificates').slick({
     infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 1,
+    slidesToScroll: 4,
     arrows: false,
-    dots: true
+    dots: true,
+    responsive: [
+        {
+            breakpoint: 991,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+
+    ]
 });
 $('.detail-slider').slick({
     infinite: true,
